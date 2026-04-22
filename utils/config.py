8847717d1132f0bbef10e9a7f570b9c8afc98869
Troy Cooper <ctroy978@gmail.com>
2026-04-21 18:49:51 -0700

    removing supabase artifacts

--- utils/config.py
+++ utils/config.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
-SUPABASE_ANON_KEY = os.environ.get("SUPABASE_ANON_KEY", "")
-SUPABASE_SERVICE_ROLE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "")
 STORAGE_SECRET = os.environ["STORAGE_SECRET"]
 SESSION_CODE_SECRET = os.environ["SESSION_CODE_SECRET"]
 TEACHER_EMAILS = [e.strip().lower() for e in os.environ.get("TEACHER_EMAILS", "").split(",") if e.strip()]
